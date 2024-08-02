760d4134b0c7500b4a01cc1ee3bd2954cb50c59d
Brayan Antonio <112229331+brayanalmengor04@users.noreply.github.com>
2024-08-02 16:34:24 -0500

    change class  servlet

--- Collection/HolaMundoJavaWeb/src/main/java/gm/com/mx/HolaMundo.java
+++ Collection/HolaMundoJavaWeb/src/main/java/gm/com/mx/HolaMundo.java
@@ -4,14 +4,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 import java.io.IOException;
 
-@WebServlet("/HolaMundoServlet") //http:8080/HolaMundoJavaWeb/HolaMundoServlet
+// Nombre del servlets
+@WebServlet("/Servlet") //http:8080/HolaMundoJavaWeb/Servlet asi se mostraria en lla web 
 public class HolaMundo extends HttpServlet{
     
-    @Override
+    @Override                //eL MTODO DOGET peticion - Respuesta  
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException{
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/html;charset=UTF-8"); // El tipo de respuesta HTML 
         var salida = response.getWriter();
-        salida.print("Hola Mundo desde Servlets");
+        salida.print("Hola mundo salida del servlets a la web");
     }
 }
